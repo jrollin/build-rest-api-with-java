f4760722b09e3fcce43efebecc39b4928b92f9cc
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-01-31 18:28:25 +0100

    test with SpringBootTest

--- src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
+++ src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
@@ -1,5 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.talanlabs.training.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class VersionRestControllerTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void getVersion() {
+
+        String response = this.restTemplate.getForObject("http://localhost:" + port + "/api/version",
+                String.class);
+        assertThat(response).contains("version");
+    }
 }

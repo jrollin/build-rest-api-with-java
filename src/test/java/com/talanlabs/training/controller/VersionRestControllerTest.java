9f2d3d255c02a23e7f0a096564ceb3b399dbd1e3
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-01-31 18:32:34 +0100

    test with SpringBootTest and AutoConfigureMockMvc

--- src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
+++ src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
@@ -2,26 +2,28 @@ package com.talanlabs.training.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class VersionRestControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    void getVersion() {
-
-        String response = this.restTemplate.getForObject("http://localhost:" + port + "/api/version",
-                String.class);
-        assertThat(response).contains("version");
+    public void shouldReturnVersion() throws Exception {
+        this.mockMvc.perform(
+                get("/api/version"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("version")));
     }
 }

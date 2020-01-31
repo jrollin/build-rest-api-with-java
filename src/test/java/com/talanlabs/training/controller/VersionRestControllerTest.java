69a779ffb72b86061774edc1bc965e12e8e83d4c
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-01-31 18:48:04 +0100

    use jsonPath to assert json response

--- src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
+++ src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
@@ -3,14 +3,15 @@ package com.talanlabs.training.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(VersionRestController.class)
+@TestPropertySource(properties = {"version = 1.4"})
 class VersionRestControllerTest {
 
     @Autowired
@@ -21,6 +22,6 @@ class VersionRestControllerTest {
         this.mockMvc.perform(
                 get("/api/version"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("version")));
+                .andExpect(jsonPath("$.version").value("1.4"));
     }
 }

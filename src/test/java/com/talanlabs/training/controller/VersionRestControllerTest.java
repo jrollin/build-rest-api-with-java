da09c228c7a713c4c5c925f2895fc6ece692f476
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-01-31 18:35:33 +0100

    test with WebMvcTest

--- src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
+++ src/test/java/com/talanlabs/training/controller/VersionRestControllerTest.java
@@ -2,18 +2,15 @@ package com.talanlabs.training.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(VersionRestController.class)
 class VersionRestControllerTest {
 
     @Autowired

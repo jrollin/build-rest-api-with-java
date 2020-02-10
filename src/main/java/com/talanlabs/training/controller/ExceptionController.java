0a7346af6093c7abea5ca3b81f05a9f0284ecf44
julien rollin <1086565+jrollin@users.noreply.github.com>
2020-02-10 23:30:24 +0100

    feat: use specific controller to manage exceptions

---
+++ src/main/java/com/talanlabs/training/controller/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.talanlabs.training.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@ControllerAdvice
+@ResponseBody
+public class ExceptionController {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
--- src/main/java/com/talanlabs/training/controller/command/ArticleCommandRestController.java
+++ src/main/java/com/talanlabs/training/controller/command/ArticleCommandRestController.java
@@ -5,14 +5,10 @@ import com.talanlabs.training.application.command.PublishArticleCommand;
 import com.talanlabs.training.application.command.SubmitArticleCommand;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(produces = {"application/json;charset=utf-8"})
@@ -38,16 +34,4 @@ public class ArticleCommandRestController {
         articleCommandUseCase.publishArticle(publishArticleCommand);
     }
 
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
 }
